f5ac3e3c6ccada157d727be041373af57be167a9
D'Angelo Gonzalez <53575762+EnigmaMe@users.noreply.github.com>
2020-02-17 13:32:04 -0800

    The actual code

---
+++ smtpcheck.py
@@ -0,0 +1,16 @@
+
+#SMTPCheck Developed by @BeefOverflow
+import smtplib
+print("SMTPCheck - Checks the authenticity of SMTP credentials against a host")
+host = raw_input("\nEnter your hostname:\n")
+username = raw_input("Enter your username:\n")
+password = raw_input("Enter your password\n")
+print("\n[*] Connecting to SMTP Server")
+server = smtplib.SMTP(host)
+print("\033[33mConnected with the following:\n")
+print "\033[32mHostname: ", host
+print "\nUsername: ", username
+print "\nPassword: ", password
+print("\n\033[31m[*]SERVER RESPONSE:\033[0m\n")
+print(server.login(username,password))
+server.quit()
